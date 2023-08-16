bdcd304b1c33ffe93000df24830e9708046e3acd
Qasim Ali <qasim.ali@healthwire.pk>
2023-08-16 22:15:56 +0500

    - error when creating ‘prompt’ - user sign out not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :prompts
-         has_many :comments
-         before_create :randomize_id
-         private
-         def randomize_id
-          begin
-            self.id = SecureRandom.random_number
-            (1_000_000_000)
-          end while User.where(id: self.id).exists?
-        end
+  has_many :prompts
+  has_many :comments
+  before_create :randomize_id
 
+  private
+    def randomize_id
+      begin
+        self.id = SecureRandom.random_number(1_000_000_000)
+      end while User.where(id: self.id).exists?
+    end
 end
